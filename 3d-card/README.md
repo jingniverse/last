e7d697ee24adf85a4250a9be3658fa0728c2e112
Yeong Hwan Kim <kch8317@gmail.com>
2023-11-25 18:43:05 +0900

    anjwl

--- 3d-card/README.md
+++
@@ -1,5 +0,0 @@
-# 3D Card
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/gayane-gasparyan/pen/wvxewXO](https://codepen.io/gayane-gasparyan/pen/wvxewXO).
-
-3D effect on card hover
--- html5_7th-master/36.effect/js/txtAni.js
+++
@@ -1,9 +0,0 @@
-function textAnimation(selector,option){
-    let tag = document.querySelector(selector)
-    let txt = tag.textContent
-    let result = ""
-    for(let i=0 ; i<txt.length ; i++){
-        result+="<i style='animation:"+(option.aniName)+" 1s ease "+(i*option.delay)+"s forwards;opacity:0;display:inline-block'>"+txt[i]+"</i>"
-    }
-    tag.innerHTML = result
-}
