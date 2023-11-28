7ae421d8aa1e2a56a3840f265aeaade85a01a33d
김영환 <kch8317@gmail.com>
2023-11-28 12:36:01 +0900

    태블릿 수정

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -15,4 +15,41 @@ icons.forEach (icon => {
   $('.plan_popup').removeClass('on')
 })
 
+let btn = $('.btn_next2')
+let btn2 = $('.btn_prev2')
+let btn3 = $('.btn_next3')
+let btn4 = $('.btn_prev3')
+let train = $('.train2')
+let train2 = $('.train3')
+
+let count=0
+
+btn.click(function(e){
+    e.preventDefault()
+    count++
+    if(count>2){count=0}
+     train.css('transform','translateX('+(-50*count)+'%)')
+})
+btn2.click(function(e){
+  e.preventDefault()
+  count--
+  if(count<0){count=2}
+   train.css('transform','translateX('+(-50*count)+'%)')
+})
+
+btn3.click(function(e){
+  e.preventDefault()
+  count++
+  if(count>1){count=0}
+   train2.css('transform','translateX('+(-50*count)+'%)')
+})
+btn4.click(function(e){
+e.preventDefault()
+count--
+if(count<0){count=1}
+ train2.css('transform','translateX('+(-50*count)+'%)')
+})
+
+
+
 })
